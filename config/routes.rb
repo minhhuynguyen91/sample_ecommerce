fe32353c4d8624193386710a2b7f835c2023375b
Minh Nguyen <ng.huyminh91@gmail.com>
2018-09-04 21:34:28 +0700

    Update backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'homes/index'
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
+  get '/cart', to: 'carts#show'
+  put 'add/:product_id', to: 'carts#add', as: :add_to
+  put 'remove/:product_id', to: 'carts#remove', as: :remove_from
+
+  root 'homes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
